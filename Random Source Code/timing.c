e159b4bf0adc6b2f824439d20183660a906df0e0
thorp1235 <will.thorp1235@gmail.com>
2022-04-09 14:16:04 +1000

    starting repo again lol

---
+++ Random Source Code/timing.c
@@ -0,0 +1,11 @@
+#include <mc9s12dp256.h>        /* derivative information */
+
+#include "timing.h"
+
+
+void delay_ms(unsigned int time){
+  int i;
+  int j;
+    for(i=0;i<time;i++)
+      for(j=0;j<4000;j++);
+}
---
+++ Random Source Code/timing.h
@@ -0,0 +1,11 @@
+#ifndef TIMING
+#define TIMING
+
+
+
+//creates a delay of 1 ms in the code
+void delay_ms(unsigned int time);
+
+
+
+#endif
