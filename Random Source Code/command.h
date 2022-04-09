78e84dab1c617b4c9f831d42fe0522587c8cfd83
H-TCF <63900249+H-TCF@users.noreply.github.com>
2022-04-09 14:37:00 +1000

    led stuff

--- Random Source Code/command.h
+++ Random Source Code/command.h
@@ -14,6 +14,12 @@ void run_instruction(char *instruction, int length);
 //functions to execute changing the seven seg
 void sevensegmodule(int number);
 
+//function to turn leds on
+void ledOn(void);
+
+//function to turn leds off
+void ledOff(void);
+
 
 
 
